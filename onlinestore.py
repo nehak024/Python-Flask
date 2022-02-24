b77abf1a0678207d02dc1f2792b3b99459bf3588
nehak024 <nbkavatage24697@gmail.com>
2022-02-25 00:34:55 +0530

    REST-API implementation

--- onlinestore.py
+++ onlinestore.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 stores = [
@@ -17,13 +17,23 @@ stores = [
 # POST /store data:{name:}
 @app.route('/store', methods=['POST'])
 def create_store():
-    pass
+    request_data = request.get_json()
+    new_store = {
+        'name': request_data['name'],
+        'items': []
+    }
+    stores.append(new_store)
+    return jsonify(new_store)
 
 
 # GET /store/<string:name>
 @app.route('/store/<string:name>')
 def get_store(name):
-    pass
+    # iterate over stores if the store matches return it or else return error message
+    for store in stores:
+        if store['name'] == name:
+            return jsonify(store)
+    return jsonify({'message': 'store not found'})
 
 
 # GET /store - Get all stores
@@ -35,13 +45,25 @@ def get_stores():
 # POST /store/<string:name>/item {name:,price:}
 @app.route('/store/<string:name>/item', methods=['POST'])
 def create_item_in_store(name):
-    pass
+    request_data = request.get_json()
+    for store in stores:
+        if store['name'] == name:
+            new_item = {
+                'name': request_data['name'],
+                'price': request_data['price']
+            }
+        stores.append(new_item)
+        return jsonify(new_item)
+    return jsonify({'message': 'item not found'})
 
 
 # GET /store/<string:name>/item {name:,price:}
 @app.route('/store/<string:name>/item')
 def get_items_in_store(name):
-    pass
+    for store in stores:
+        if store['name'] == name:
+            return jsonify({'items': store['items']})
+    return jsonify({'message': 'store not found'})
 
 
 app.run(port=5000)
